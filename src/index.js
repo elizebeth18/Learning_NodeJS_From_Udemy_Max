1742d8a0321719e8a1ee3ba9d4621c9588668a6c
Jilu Elizebeth <elizebeth18.jilu@gmail.com>
2026-07-30 23:19:44 +0530

    styling

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ const app = express();
 //Define paths for Express config
 const publicDirectoryPath = path.join(__dirname, '../public');
 const viewsPath = path.join(__dirname, '../templates/views');
-const partialsPath = path.join(__dirname,'../templates/partials');
+const partialsPath = path.join(__dirname, '../templates/partials');
 
 //setup handlebars engine and views location
 app.set('view engine', 'hbs');
@@ -17,7 +17,16 @@ app.set('views', viewsPath);
 hbs.registerPartials(partialsPath);
 
 //setup static directory to serve
-app.use(express.static(publicDirectoryPath))
+app.use(express.static(publicDirectoryPath));
+
+
+
+app.get('/', (req, res, next) => {
+    res.render('index', {
+        title: 'Weather',
+        name: 'Jilu Elizebeth'
+    })
+});
 
 app.get('/help', (req, res, next) => {
     res.render('help', {
@@ -35,16 +44,31 @@ app.get('/about', (req, res, next) => {
     })
 });
 
+
 app.get('/weather', (req, res, next) => {
-    res.send({ forcast: "it is cloudy", temperature: "20.5 C", location: "Bangalore" })
+    res.send({
+        forcast: "it is cloudy",
+        temperature: "20.5 C",
+        location: "Bangalore"
+    });
 });
 
-app.get('', (req, res, next) => {
-    res.render('index', {
-        title: 'Weather',
-        name: 'Jilu Elizebeth'
-    })
+app.get('/help/{*splat}', (req, res) => {
+    res.render('404', {
+        title: 404,
+        name: 'Jilu Elizebeth',
+        errorMessage: 'Help article not found'
+    });
 });
+
+app.get('/{*splat}', (req, res) => {
+    res.render('404',{
+        title: 404,
+        name: 'Jilu Elizebeth',
+        errorMessage: 'Page Not Found'
+    });
+});
+
 
 app.listen(3000, () => {
     console.log('Listen to port number 3000')
