5877fa4e121e06301aafc61d90b238bf0f1f8203
Jilu Elizebeth <elizebeth18.jilu@gmail.com>
2026-07-30 18:45:11 +0530

    implement partials

--- src/index.js
+++ src/index.js
@@ -2,15 +2,27 @@ const path = require('node:path');
 
 //import express
 const express = require('express');
+const hbs = require('hbs');
 
 const app = express();
-const publicDirectoryPath = path.join(__dirname, '../public')
 
-app.set('view engine', 'hbs')
+//Define paths for Express config
+const publicDirectoryPath = path.join(__dirname, '../public');
+const viewsPath = path.join(__dirname, '../templates/views');
+const partialsPath = path.join(__dirname,'../templates/partials');
+
+//setup handlebars engine and views location
+app.set('view engine', 'hbs');
+app.set('views', viewsPath);
+hbs.registerPartials(partialsPath);
+
+//setup static directory to serve
 app.use(express.static(publicDirectoryPath))
 
 app.get('/help', (req, res, next) => {
     res.render('help', {
+        title: "Help",
+        name: "Jilu",
         helpMessage: 'This is a help message'
     })
 });
@@ -18,11 +30,10 @@ app.get('/help', (req, res, next) => {
 
 app.get('/about', (req, res, next) => {
     res.render('about', {
+        title: "About",
         name: "Andrew"
     })
-})
-
-
+});
 
 app.get('/weather', (req, res, next) => {
     res.send({ forcast: "it is cloudy", temperature: "20.5 C", location: "Bangalore" })
